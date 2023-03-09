99b891f78df09f1c2a95e2957659b97254f628d1
Elia Savino <elia.savino@me.com>
2023-03-09 14:26:15 +0100

    Package is complete

--- Testing.py
+++ Testing.py
@@ -1,11 +1,9 @@
 
 from Ramacrompy import Spectra
+import os
+
+Spec = Spectra('DataFiles/20230301/Example_Raw_Kinetic.sif')
 
 
-Spec = Spectra('./DataFiles/20230301/TRC_434_M4_before.sif')
-Spec2 = Spectra('./DataFiles/20230301/TRC_434_M4_after.sif')
-Spec.integrate(start=650.0, end = 800.0)
-Spec2.integrate(start=1000.0, end = 1030.0)
-
-Spec.plot_integral_single(other_spectra=[Spec2], labels=['jeofrry'])
+Spec.plot_kinetic()
 
